b5f984c0252d4799080057470052eb4a280ad800
Glayson Cardoso <glaysoncardoso7@gmail.com>
2023-10-17 16:48:46 -0300

    Implementação das views, templates e painel administrativo

--- my_blog/blog_app/views.py
+++ my_blog/blog_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all().order_by("-published_date")
+    return render(request, "homepage.html", {"posts": posts})
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "post_detail.html", {"post": post})
